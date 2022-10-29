6a7d5930519e7e72bcd2f290b2de094faa82cd38
Charlie <clee@tuta.io>
2022-10-29 15:19:02 -0700

    specify couchbase namespace for couchbase endpoint

--- temporal-adventure-bot/deploy.example.cmd
+++ temporal-adventure-bot/deploy.example.cmd
@@ -5,8 +5,8 @@ set COUCHBASE_PASSWORD=the_COUCHBASE_PASSWORD
 cd C:\git\temporal-adventure-bot
 docker-compose -f C:\git\temporal-adventure-bot\docker-compose.yaml -f C:\git\setup-neonkube\temporal-adventure-bot\docker-compose.yaml build
 
-docker tag 192.168.1.151:32000/temporal-adventure-bot:1.22.128 neon-registry.18e7-091a-7bb4-d81e.neoncluster.io/leenet/temporal-adventure-bot:1.22.128
-docker push neon-registry.18e7-091a-7bb4-d81e.neoncluster.io/leenet/temporal-adventure-bot:1.22.128
+docker tag 192.168.1.151:32000/temporal-adventure-bot:1.22.129 neon-registry.18e7-091a-7bb4-d81e.neoncluster.io/leenet/temporal-adventure-bot:1.22.129
+docker push neon-registry.18e7-091a-7bb4-d81e.neoncluster.io/leenet/temporal-adventure-bot:1.22.129
 
 cd C:\Users\alice\.neonkube\tools
 helm install temporal-adventure-bot --set couchbasePassword=%COUCHBASE_PASSWORD% --set slackBotToken=%SLACK_BOT_TOKEN% --set slackSigningSecret=%SLACK_SIGNING_SECRET% -f C:\git\setup-neonkube\temporal-adventure-bot\values.yaml C:\git\temporal-adventure-bot\temporal-adventure-bot --namespace leenet
