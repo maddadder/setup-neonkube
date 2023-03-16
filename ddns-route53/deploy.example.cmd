3c7cf84fae61239daa5a522fa633164a747b8be5
Charlie Lee <clee@tuta.io>
2023-03-16 13:24:27 -0700

    update deploy script

--- ddns-route53/deploy.example.cmd
+++ ddns-route53/deploy.example.cmd
@@ -1,5 +1,6 @@
 set DDNSR53_CREDENTIALS_ACCESSKEYID=YOUR_ROUTE53_KEY_ID
 set DDNSR53_CREDENTIALS_SECRETACCESSKEY=YOUR_ROUTE53_KEY_SECRET
 
+git clone https://github.com/maddadder/ddns-route53.git
 cd C:\Users\alice\.neonkube\tools
-helm install ddns-route53 --set Route53AccessKeyId=%DDNSR53_CREDENTIALS_ACCESSKEYID% --set Route53SecretAccessKey=%DDNSR53_CREDENTIALS_SECRETACCESSKEY% C:\git\ddns-route53\ddns-route53 --namespace leenet
+helm install ddns-route53 --namespace leenet --set Route53AccessKeyId=%DDNSR53_CREDENTIALS_ACCESSKEYID% --set Route53SecretAccessKey=%DDNSR53_CREDENTIALS_SECRETACCESSKEY% C:\git\ddns-route53\ddns-route53 --namespace leenet
--- ddns-route53/deploy.example.sh
+++ ddns-route53/deploy.example.sh
@@ -1,4 +1,6 @@
 export DDNSR53_CREDENTIALS_ACCESSKEYID=YOUR_ROUTE53_KEY_ID
 export DDNSR53_CREDENTIALS_SECRETACCESSKEY=YOUR_ROUTE53_KEY_SECRET
 
-helm install ddns-route53 --set Route53AccessKeyId=$DDNSR53_CREDENTIALS_ACCESSKEYID --set Route53SecretAccessKey=$DDNSR53_CREDENTIALS_SECRETACCESSKEY ~/git3/ddns-route53/ddns-route53 --namespace leenet
+git clone https://github.com/maddadder/ddns-route53.git
+cd ddns-route53
+helm install ddns-route53 --namespace leenet --set Route53AccessKeyId=$DDNSR53_CREDENTIALS_ACCESSKEYID --set Route53SecretAccessKey=$DDNSR53_CREDENTIALS_SECRETACCESSKEY ~/git3/ddns-route53/ddns-route53 --namespace leenet
